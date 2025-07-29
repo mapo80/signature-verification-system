ada99e5b2dd5c98f1533b17ead05b7769b72eb0f
Matteo Polito <mapo80@gmail.com>
2025-07-29 21:28:30 +0200

    Use relative paths for model files

--- SignatureVerification.Api/Program.cs
+++ SignatureVerification.Api/Program.cs
@@ -3,7 +3,7 @@ using SignatureVerification.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var soDir = Path.GetFullPath(Path.Combine(builder.Environment.ContentRootPath, "..", "sigver", "so"));
+var soDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "sigver", "so"));
 var ld = Environment.GetEnvironmentVariable("LD_LIBRARY_PATH");
 Environment.SetEnvironmentVariable("LD_LIBRARY_PATH", string.IsNullOrEmpty(ld) ? soDir : $"{soDir}:{ld}");
 
@@ -11,20 +11,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<SignatureDetectionService>(sp =>
+builder.Services.AddSingleton<SignatureDetectionService>(_ =>
 {
-    var env = sp.GetRequiredService<IHostEnvironment>();
-    var basePath = Path.Combine(env.ContentRootPath, "..", "signature-detection");
-    var detrPath = Path.GetFullPath(Path.Combine(basePath, "conditional_detr_signature.onnx"));
-    var yoloPath = Path.GetFullPath(Path.Combine(basePath, "yolov8s.onnx"));
+    var basePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "signature-detection"));
+    var detrPath = Path.Combine(basePath, "conditional_detr_signature.onnx");
+    var yoloPath = Path.Combine(basePath, "yolov8s.onnx");
     return new SignatureDetectionService(detrPath, yoloPath);
 });
 
 builder.Services.AddSingleton<SignatureVerificationService>(sp =>
 {
-    var env = sp.GetRequiredService<IHostEnvironment>();
     var detector = sp.GetRequiredService<SignatureDetectionService>();
-    var modelPath = Path.GetFullPath(Path.Combine(env.ContentRootPath, "..", "sigver", "models", "signet.onnx"));
+    var modelPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "sigver", "models", "signet.onnx"));
     return new SignatureVerificationService(detector, modelPath);
 });
 
