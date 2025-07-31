48ebe99fe73327639074e8a584c3fab7a94d883d
Matteo Polito <mapo80@gmail.com>
2025-07-31 14:48:58 +0200

    Use relative API base URL for webapp

--- webapp/src/api/core/OpenAPI.ts
+++ webapp/src/api/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: 'http://localhost:5217',
+    BASE: '',
     VERSION: '1.0.0',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
